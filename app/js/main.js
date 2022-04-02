5dcbc706cf72b36888d165e27d990790ec4f6723
Avircut <lomoff.slaw@yandex.ru>
2022-04-02 22:00:11 +0700

    Resolutions 320px 480px 620px added

--- app/js/main.js
+++ app/js/main.js
@@ -18,5 +18,35 @@ acc.forEach( el => {
 })
 $('.clients__slider-container').slick({
   arrows:true,
-  autoplay:false
+  autoplay:false,
+  responsive:[
+    {
+      breakpoint:600,
+      settings:{
+        slidesToShow:1,
+        slidesToScroll:1
+      }
+    },
+    {
+      breakpoint:800,
+      settings:{
+        slidesToShow:2,
+        slidesToScroll:2
+      }
+    },
+    {
+      breakpoint:1200,
+      settings:{
+        slidesToShow:3,
+        slidesToScroll:3
+      }
+    },
+    {
+      breakpoint:1600,
+      settings:{
+        slidesToShow:4,
+        slidesToScroll:4
+      }
+    },
+  ]
 })

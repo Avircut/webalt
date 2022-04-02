99eb235efd61d663742a91976bc74893708e7bf3
Avircut <lomoff.slaw@yandex.ru>
2022-04-02 12:23:48 +0700

    header and stages sections added

---
+++ app/js/main.js
@@ -0,0 +1,18 @@
+let acc = document.querySelectorAll(".stages__item");
+
+acc.forEach( el => {
+  el.addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("stages__item_active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.lastChild.previousSibling;
+    console.log(panel);
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight= null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight +"px";
+    }
+  });
+})
